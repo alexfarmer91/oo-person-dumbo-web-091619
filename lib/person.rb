db3a9a43922df9c8dc3fdf84acd7b48ec0a8ccf5
Alex Farmer <farmale10@gmail.com>
2019-09-18 18:41:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,16 @@
 
 class Person 
   
+  attr_reader :name 
+  attr_writer
+  attr_accessor
   
+  def initialize(name)
+    @name = name 
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end 
   
   
   
